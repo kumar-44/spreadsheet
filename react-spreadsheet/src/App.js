f693013c43a82764a9bb66fd44ad31d73b12efbb
kumar-44 <149786913+kumar-44@users.noreply.github.com>
2025-07-07 16:07:49 +0530

    Add files via upload

---
+++ react-spreadsheet/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Toolbar from './components/Toolbar';
+import Spreadsheet from './components/Spreadsheet';
+
+function App() {
+  return (
+    <div className="App">
+      <Toolbar />
+      <Spreadsheet />
+    </div>
+  );
+}
+
+export default App;
